4d377017a00f921be8154e64f6726f4b85752c43
uh-sman <75017192+uh-sman@users.noreply.github.com>
2023-10-08 12:11:03 -0700

    updatde

--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require('cors')
 const app = express();
 // const { DEVELOPMENT } = process.env;
 const PORT = 4000 || process.env.DEVELOPMENT;
+const corsOption = {
+  origin:['https://dcanestate.onrender.coms']
+}
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", require("./routes/userRoutes"));
+app.use("/", cors(corsOption),require("./routes/userRoutes"));
 app.use("/listing", require("./routes/listings/index"));
-
+app.use(cors(corsOption))
+app.use(cors())
 app.listen(PORT, () => {
   console.log({ message: "hello" });
 });
