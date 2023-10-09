3fbaf9006ba70550720d6762efed52ae76ce0731
uh-sman <75017192+uh-sman@users.noreply.github.com>
2023-10-09 02:47:37 -0700

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -4,19 +4,24 @@ const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const cors = require('cors')
 const app = express();
+app.use(cors({
+  origin: ["*",'https://dcanestate.onrender.com'],
+}))
 // const { DEVELOPMENT } = process.env;
 const PORT = 4000 || process.env.DEVELOPMENT;
-const corsOption = {
-  origin:['https://dcanestate.onrender.coms']
-}
+// const whitelist =["*", ] 
+// const whitelist =["*"] 
+
+  // origin:['https://dcanestate.onrender.com', 'http://localhost:4000', 'http://localhost:3000'],
+  
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", cors(corsOption),require("./routes/userRoutes"));
+app.use("/", require("./routes/userRoutes"));
 app.use("/listing", require("./routes/listings/index"));
-app.use(cors(corsOption))
-app.use(cors())
+// app.use(cors(corsOption))
+// app.use(cors())
 app.listen(PORT, () => {
   console.log({ message: "hello" });
 });
